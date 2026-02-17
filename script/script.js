311e53c385114ef46620b22615a66f15a2395b1a
Pedro D. <115499642+peuzada2@users.noreply.github.com>
2026-02-17 16:16:17 -0300

    att

--- script/script.js
+++ script/script.js
@@ -5,3 +5,22 @@ function openMenu() {
 function closeMenu() {
   document.getElementById("mobileMenu").classList.remove("active");
 }
+
+function toggleCatalog(button) {
+  const content = button.nextElementSibling;
+  const icon = button.querySelector("span");
+
+  if (content.style.maxHeight) {
+    content.style.maxHeight = null;
+    icon.textContent = "+";
+  } else {
+    document.querySelectorAll(".catalog-content").forEach(item => {
+      item.style.maxHeight = null;
+    });
+    document.querySelectorAll(".catalog-header span").forEach(i => i.textContent = "+");
+
+    content.style.maxHeight = content.scrollHeight + "px";
+    icon.textContent = "−";
+  }
+}
+
